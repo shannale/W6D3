0bf84eea3f1fcca1bc52660434a1d6eb0a22622a
Shanna Le <shannatle@gmail.com>
2023-05-03 14:50:17 -0700

    Finish Phase 1

--- practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :users
+  
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#index'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 
   # Defines the root path route ("/")
   # root "articles#index"
