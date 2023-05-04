b810f58c6c09f4660beb40a738da21fe5d0b4739
nj <nickgentrymma@gmail.com>
2023-05-04 09:24:45 -0700

    uncommented our methods

--- practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/app/controllers/users_controller.rb
@@ -6,25 +6,34 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(params.require(:user).permit(:name, :email))
-        @user.save!
-        render json: @user
-        # user = User.new(params.require(:user).permit(:name, :email))
-        # if @user.save
-        #   render json: user
-        # else
-        #   render json: user.errors.full_messages, status: :unprocessable_entity
-        # end
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else  
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
-
+    
     def show
-        # @user = User.find(params[:id])
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
 
-    # def user_params
-    #     params.require(:user).permit()
-    # end
+    def update
+
+    end
+
+    def destroy
+        
+    end
+    
+    
+
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 
     
 
