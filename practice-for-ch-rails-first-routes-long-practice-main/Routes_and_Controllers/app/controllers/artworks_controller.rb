2a5bc6d6916fba30be7b7eb4b4e886e759f34ec2
Shanna Le <shannatle@gmail.com>
2023-05-04 14:04:21 -0700

    Finish Phase 2 Art API

---
+++ practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/app/controllers/artworks_controller.rb
@@ -0,0 +1,57 @@
+class ArtworksController < ApplicationController
+    
+    def index 
+        @artworks = Artwork.all 
+        render json: @artworks
+    end 
+
+    def create 
+        @artwork = Artwork.new(artwork_params)
+        if @artwork.save 
+            render json: @artwork 
+        else 
+            render json: @artwork.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+
+    def show 
+        @artwork = Artwork.find(params[:id])
+        render json: @artwork 
+    end 
+
+    def update 
+        @artwork = Artwork.find(params[:id])
+        if @artwork.update(artwork_params)
+            render json: @artwork 
+        else 
+            render json: @artwork.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+
+    def destory 
+        @artwork = Artwork.find(params[:id])
+        @artwork.destory 
+        render json: @user 
+    end 
+
+    private 
+
+    def artwork_params 
+        params.require(:artwork).permit(:title, :image_url, :artist_id)
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
