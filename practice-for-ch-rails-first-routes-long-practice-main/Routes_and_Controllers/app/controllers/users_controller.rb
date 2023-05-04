5e84fe4abbd2f24fab46ea0bd8d5735c8731ae23
Shanna Le <shannatle@gmail.com>
2023-05-04 09:42:10 -0700

    Finish Routes And Controllers Project

--- practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/Routes_and_Controllers/app/controllers/users_controller.rb
@@ -20,22 +20,26 @@ class UsersController < ApplicationController
     end
 
     def update
-
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: @user 
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end 
     end
 
     def destroy
-        
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: @user
     end
     
     
-
-
     private
+
     def user_params
         params.require(:user).permit(:name, :email)
     end
 
     
-
-
 end
